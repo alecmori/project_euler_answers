56d19956c4e5de34f712dcdc3c7dc550a79d7568
alecmori <alecmori@gmail.com>
2019-02-08 11:02:05 -0500

    Problem 26 test

---
+++ answers/problem_26/run_problem.pxd
@@ -0,0 +1,1 @@
+cpdef unsigned int run_problem()
---
+++ answers/problem_26/run_problem.pyx
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+cpdef unsigned int run_problem():
+    pass
+
+
+if __name__ == '__main__':
+    answer = run_problem()
+    #TODO
+    if answer == -1:
+        print('Correct!')
+    else:
+        print('Incorrect!')
