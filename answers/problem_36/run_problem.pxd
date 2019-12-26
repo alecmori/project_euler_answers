2430feff4d9cbc521ef224430f35c17cdf14f450
Alec Mori <alecmori@gmail.com>
2019-12-26 01:05:50 -0500

    Problem 36 but much faster

--- answers/problem_36/run_problem.pxd
+++ answers/problem_36/run_problem.pxd
@@ -3,7 +3,8 @@ import cython
 @cython.locals(total=cython.uint, num=cython.uint, s_num=str, b_num=str)
 cpdef unsigned int run_problem(unsigned int n=*)
 
-@cython.locals(start=cython.uint, end=cython.uint, string=str)
-cpdef cython.bint _is_palindrome(
-    str string, unsigned int start=*, unsigned int end=*,
-)
+@cython.locals(rev=cython.uint, orig_num=cython.uint)
+cpdef cython.bint _is_palindrome(unsigned int num)
+
+@cython.locals(rev=cython.uint, orig_num=cython.uint)
+cpdef cython.bint _is_bin_palindrome(unsigned int num)
--- answers/problem_36/run_problem.py
+++ answers/problem_36/run_problem.py
@@ -2,24 +2,30 @@
 def run_problem(n=1000000):
     total = 0
     for num in range(1, n, 2):
-        s_num = str(num)
-        if not _is_palindrome(s_num, start=0, end=len(s_num) - 1):
+        if not _is_palindrome(num):
             continue
-        b_num = bin(num)
-        if not _is_palindrome(b_num, start=2, end=len(b_num) - 1):
+        if not _is_bin_palindrome(num):
             continue
         total += num
     return total
 
-def _is_palindrome(string, start=0, end=0):
-    if end == 0:
-        end = len(string) - 1
-    while end > start:
-        if string[end] != string[start]:
-            return False
-        end -= 1
-        start += 1
-    return True
+def _is_palindrome(num):
+    rev = 0
+    orig_num = num
+    while num > 0:
+        rev *= 10
+        rev += num % 10
+        num = num // 10
+    return rev == orig_num
+
+def _is_bin_palindrome(num):
+    rev = 0
+    orig_num = num
+    while num > 0:
+        rev = rev << 1
+        rev = rev | (num & 1)
+        num = num >> 1
+    return rev == orig_num
 
 if __name__ == '__main__':
     answer = run_problem()
